95af25ef74e3fda9a40a5f2869a3c0b5eac84ebc
Md Nifad Uzzaman <nifaduzzaman2005@gmail.com>
2024-10-22 11:04:34 +0600

    Basic stracture created

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div className='flex h-screen w-screen justify-center items-center text-4xl'>
+      Not found
+      
+    </div>
+  )
+}
+
+export default NotFound;
